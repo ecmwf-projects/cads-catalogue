53e17e6513b324c72422a9733fc0ddc46c4ac833
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2022-06-22 18:32:46 +0200

    bugfix: more prints

--- cads_catalogue/entry_points.py
+++ cads_catalogue/entry_points.py
@@ -20,7 +20,7 @@ def info(connection_string: str) -> str:
     engine = sa.create_engine(connection_string)
     connection = engine.connect()
     connection.close()
-    return "successfully connected to the catalogue database."
+    print("successfully connected to the catalogue database.")
 
 
 @app.command()
@@ -31,7 +31,7 @@ def init_db(connection_string: str) -> str:
     :param connection_string: something like 'postgresql://user:password@netloc:port/dbname'
     """
     database.init_db(connection_string)
-    return "successfully created the catalogue database structure."
+    print("successfully created the catalogue database structure.")
 
 
 @app.command()
@@ -44,6 +44,7 @@ def load_test_data(connection_string: str) -> str:
     this_path = os.path.abspath(os.path.dirname(__file__))
     dumped_db_path = os.path.abspath(os.path.join(this_path, '../tests/TESTDATA/testdb.sql'))
     subprocess.call(["psql", connection_string, "-f", dumped_db_path])
+    print('test data loaded.')
 
 
 def main():
