78dd8a14a72ed504517ed44f0435e2a534bc142a
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2022-10-04 12:45:02 +0200

    update tests about env requirements

--- tests/test_01_config.py
+++ tests/test_01_config.py
@@ -8,23 +8,23 @@ from cads_catalogue import config
 
 def test_sqlalchemysettings(temp_environ: Any) -> None:
     # check settings must have a password set (no default)
-    temp_environ.pop("postgres_password", default=None)
+    temp_environ.pop("catalogue_db_password", default=None)
     with pytest.raises(ValueError) as excinfo:
         config.SqlalchemySettings()
-    assert "postgres_password" in str(excinfo.value)
+    assert "catalogue_db_password" in str(excinfo.value)
 
     # also an empty password can be set
-    settings = config.SqlalchemySettings(postgres_password="")
-    assert settings.postgres_password == ""
+    settings = config.SqlalchemySettings(catalogue_db_password="")
+    assert settings.catalogue_db_password == ""
 
     # also a not empty password can be set
-    temp_environ["postgres_password"] = "a password"
+    temp_environ["catalogue_db_password"] = "a password"
     settings = config.SqlalchemySettings()
-    assert settings.postgres_password == "a password"
+    assert settings.catalogue_db_password == "a password"
 
 
 def test_ensure_settings(session_obj: sa.orm.sessionmaker, temp_environ: Any) -> None:
-    temp_environ["postgres_password"] = "apassword"
+    temp_environ["catalogue_db_password"] = "apassword"
 
     # initially global settings is importable, but it is None
     assert config.dbsettings is None
@@ -39,14 +39,14 @@ def test_ensure_settings(session_obj: sa.orm.sessionmaker, temp_environ: Any) ->
 
     # setting a custom configuration works as well
     my_settings_dict = {
-        "postgres_user": "monica",
-        "postgres_password": "secret1",
-        "postgres_host": "myhost",
-        "postgres_dbname": "mycatalogue",
+        "catalogue_db_user": "monica",
+        "catalogue_db_password": "secret1",
+        "catalogue_db_host": "myhost",
+        "catalogue_db_name": "mycatalogue",
     }
     my_settings_connection_string = (
-        "postgresql://%(postgres_user)s:%(postgres_password)s"
-        "@%(postgres_host)s/%(postgres_dbname)s" % my_settings_dict
+        "postgresql://%(catalogue_db_user)s:%(catalogue_db_password)s"
+        "@%(catalogue_db_host)s/%(catalogue_db_name)s" % my_settings_dict
     )
     mysettings = config.SqlalchemySettings(**my_settings_dict)
     effective_settings = config.ensure_settings(mysettings)
