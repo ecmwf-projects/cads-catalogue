06de8a3169707d729447ed21eccf755dcaeff45f
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-02-07 08:43:27 +0100

    added storage settings

--- cads_catalogue/config.py
+++ cads_catalogue/config.py
@@ -16,6 +16,7 @@
 import pydantic
 
 dbsettings = None
+storagesettings = None
 
 
 class SqlalchemySettings(pydantic.BaseSettings):
@@ -48,6 +49,52 @@ class SqlalchemySettings(pydantic.BaseSettings):
             f":{self.catalogue_db_password}@{self.catalogue_db_host}"
             f"/{self.catalogue_db_name}"
         )
+
+
+class ObjectStorageSettings(pydantic.BaseSettings):
+    """Set of settings to use the object storage with the catalogue manager
+
+    - ``object_storage_url``: object storage URL (internal)
+    - ``storage_admin``: object storage admin username
+    - ``storage_password``: object storage admin password
+    - ``catalogue_bucket``: object storage bucket name to use for the catalogue metadata
+    - ``document_storage_url``: object storage URL (public)
+    """
+
+    object_storage_url: str | None = None
+    storage_admin: str | None = None
+    storage_password: str| None = None
+    catalogue_bucket: str| None = None
+    document_storage_url: str| None = None
+
+    @property
+    def storage_kws(self) -> dict[str, str | bool]:
+        """Return a dictionary to be used with the storage client"""
+        return {
+            "access_key": self.storage_admin,
+            "secret_key": self.storage_password,
+            "secure": False,
+        }
+
+
+def ensure_storage_settings(settings: ObjectStorageSettings | None = None) -> ObjectStorageSettings:
+    """If `settings` is None, create a new ObjectStorageSettings object.
+
+    Parameters
+    ----------
+    settings: an optional config.ObjectStorageSettings to be set
+
+    Returns
+    -------
+    ObjectStorageSettings:
+        a ObjectStorageSettings object
+    """
+    global storagesettings
+    if settings and isinstance(settings, ObjectStorageSettings):
+        storagesettings = settings
+    else:
+        storagesettings = ObjectStorageSettings()
+    return storagesettings
 
 
 def ensure_settings(settings: SqlalchemySettings | None = None) -> SqlalchemySettings:
