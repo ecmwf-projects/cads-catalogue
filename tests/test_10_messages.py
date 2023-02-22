e96c1b26c437ba4549fd3312d6d4dd5b3b2edc48
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-02-22 15:34:47 +0100

    fixed tests+style

--- tests/test_10_messages.py
+++ tests/test_10_messages.py
@@ -1,5 +1,5 @@
-
 import datetime
+import operator
 import os.path
 
 from cads_catalogue import messages
@@ -10,31 +10,19 @@ TEST_MESSAGE_ROOT_PATH = os.path.join(TESTDATA_PATH, "cads-messages")
 
 
 def test_load_messages() -> None:
-    msg_root = os.path.join(TESTDATA_PATH, "cads-messages")
+    msg_root = TEST_MESSAGE_ROOT_PATH
     expected_msgs = [
         {
-            "date": datetime.datetime(2023, 1, 13, 11, 27, 13),
-            "entries": ["reanalisys-era5-land", "satellite-surface-xxx"],
-            "live": False,
-            "is_global": False,
-            "message_body": "<h1>message main body for archived critical message for "
-            "some datasets</h1>\n"
-            "<p>Wider <strong>markdown syntax</strong> allowed here.</p>",
-            "status": "ongoing",
-            "severity": "critical",
-            "summary": "example of expired critical content message",
-        },
-        {
-            "date": datetime.datetime(2023, 1, 12, 11, 27, 13),
-            "entries": ["reanalisys-era5-land", "satellite-surface-radiation-budget"],
+            "date": datetime.datetime(2021, 1, 14, 11, 27, 13),
+            "entries": [""],
             "live": True,
-            "is_global": False,
-            "message_body": "<h1>message main body for active critical message for some "
-            "datasets</h1>\n"
-            "<p>Wider <strong>markdown syntax</strong> allowed here.</p>",
-            "status": "ongoing",
-            "severity": "critical",
-            "summary": "example of active critical content message",
+            "is_global": True,
+            "message_body": "<h1>main message content</h1>\n"
+            "<p>Wider <strong>markdown syntax</strong> allowed here. "
+            "This is the full text message.</p>",
+            "status": "fixed",
+            "severity": "info",
+            "summary": "a summary of the message",
         },
         {
             "date": datetime.datetime(2023, 1, 11, 11, 27, 13),
@@ -54,16 +42,28 @@ def test_load_messages() -> None:
             "summary": "",
         },
         {
-            "date": datetime.datetime(2021, 1, 14, 11, 27, 13),
-            "entries": [""],
+            "date": datetime.datetime(2023, 1, 12, 11, 27, 13),
+            "entries": ["reanalisys-era5-land", "satellite-surface-radiation-budget"],
             "live": True,
-            "is_global": True,
-            "message_body": "<h1>main message content</h1>\n"
-            "<p>Wider <strong>markdown syntax</strong> allowed here. "
-            "This is the full text message.</p>",
-            "status": "fixed",
-            "severity": "info",
-            "summary": "a summary of the message",
+            "is_global": False,
+            "message_body": "<h1>message main body for active critical message for some "
+            "datasets</h1>\n"
+            "<p>Wider <strong>markdown syntax</strong> allowed here.</p>",
+            "status": "ongoing",
+            "severity": "critical",
+            "summary": "example of active critical content message",
+        },
+        {
+            "date": datetime.datetime(2023, 1, 13, 11, 27, 13),
+            "entries": ["reanalisys-era5-land", "satellite-surface-xxx"],
+            "live": False,
+            "is_global": False,
+            "message_body": "<h1>message main body for archived critical message for "
+            "some datasets</h1>\n"
+            "<p>Wider <strong>markdown syntax</strong> allowed here.</p>",
+            "status": "ongoing",
+            "severity": "critical",
+            "summary": "example of expired critical content message",
         },
         {
             "date": datetime.datetime(2023, 1, 15, 11, 27, 13),
@@ -76,6 +76,17 @@ def test_load_messages() -> None:
             "severity": "critical",
             "summary": "A **brief description** of the message",
         },
+        {
+            "date": datetime.datetime(2023, 1, 16, 11, 27, 13),
+            "summary": "A **brief description** of the message",
+            "severity": "warning",
+            "live": True,
+            "is_global": True,
+            "message_body": "<h1>main message content</h1>\n<p>Wider <strong>"
+            "markdown syntax</strong> allowed here.</p>",
+            "status": "fixed",
+            "entries": [""],
+        },
     ]
     loaded_msgs = messages.load_messages(msg_root)
-    assert loaded_msgs == expected_msgs
+    assert sorted(loaded_msgs, key=operator.itemgetter("date")) == expected_msgs
