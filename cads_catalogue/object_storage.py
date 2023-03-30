454525160953eade3261a3820dd9160386efa635
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-03-30 11:17:27 +0200

    tests for object storage

--- cads_catalogue/object_storage.py
+++ cads_catalogue/object_storage.py
@@ -18,8 +18,9 @@ import hashlib
 import json
 import os
 import pathlib
-import urllib.parse
 from typing import Any
+import urllib.parse
+import urllib.request
 
 import minio  # type: ignore
 import structlog
@@ -40,7 +41,8 @@ DOWNLOAD_POLICY_TEMPLATE: dict[str, Any] = {
             "Resource": ["arn:aws:s3:::%(bucket_name)s"],
         },
         {
-            "Action": ["s3:GetObject"],
+            "Action": ["s3:GetObject",
+                       "s3:GetObjectVersion"],
             "Effect": "Allow",
             "Principal": {"AWS": ["*"]},
             "Resource": ["arn:aws:s3:::%(bucket_name)s/*"],
@@ -224,10 +226,10 @@ def test_bucket(
         logger.warning(f"the bucket {bucket_name} already exists")
     if not client.bucket_exists(bucket_name):
         client.make_bucket(bucket_name)
+        set_bucket_policy(client, bucket_name, "download")
         client.set_bucket_versioning(
             bucket_name, versioningconfig.VersioningConfig(commonconfig.ENABLED)
         )
-        set_bucket_policy(client, bucket_name, "download")
         logger.info(f"bucket {bucket_name} created")
     if not client.bucket_exists(bucket_name):
         logger.error(f"test 1 failed: bucket {bucket_name} not found")
@@ -248,10 +250,10 @@ def test_bucket(
     )
     subpath = "testfiles"
     file_name = os.path.basename(file_path)
-    object_name = os.path.join(subpath, file_name)
-    existing_objects = client.list_objects(
+    object_name = file_name  # os.path.join(subpath, file_name)
+    existing_objects = list(client.list_objects(
         bucket_name, object_name, include_user_meta=True, include_version=True
-    )
+    ))
     if existing_objects:
         for existing_object in existing_objects:
             version_id = existing_object.version_id
@@ -259,22 +261,24 @@ def test_bucket(
                 f"found {object_name} stored with version id: {version_id}. Try removing."
             )
             client.remove_object(bucket_name, object_name, version_id)
-    existing_objects = client.list_objects(
-        bucket_name, object_name, include_user_meta=True, include_version=True
-    )
+    existing_objects = list(client.list_objects(
+            bucket_name, object_name, include_user_meta=True, include_version=True
+        ))
     if existing_objects:
-        logger.error(f"test 2 failed: object {bucket_name} was not removed")
+        logger.error(f"test 2 failed: object removal unsuccessful")
         return
     with open(file_path, "rb") as fp:
         text = fp.read()
         source_sha256 = hashlib.sha256(text).hexdigest()
+        fp.seek(0)
+        text = fp.read()
     res = client.fput_object(
         bucket_name, object_name, file_path, metadata={"sha256": source_sha256}
     )
     version_id = res.version_id
-    existing_objects = client.list_objects(
+    existing_objects = list(client.list_objects(
         bucket_name, object_name, include_user_meta=True, include_version=True
-    )
+    ))
     if not existing_objects:
         logger.error(
             f"test 2 failed: object {object_name} with version {version_id} not uploaded/found"
@@ -336,9 +340,9 @@ def test_bucket(
             f"test 3 failed: different object {object_name} generated with the same version {version_id2}"
         )
         return
-    existing_objects = client.list_objects(
+    existing_objects = list(client.list_objects(
         bucket_name, object_name, include_user_meta=True, include_version=True
-    )
+    ))
     if not existing_objects:
         logger.error(
             f"test 3 failed: no more found {object_name} with version {version_id2}"
@@ -361,6 +365,7 @@ def test_bucket(
                 f"{existing_object.version_id} / {destination_sha256}"
             )
         else:
+            logger.info(f"found expected {object_name} with version {existing_object.version_id} and sha256 {destination_sha256}")
             found_right += 1
     if found_right == 2 and how_many_found == 2:
         logger.info("test 3 passed")
@@ -368,11 +373,38 @@ def test_bucket(
         logger.error(
             f"test 3 failed: found {found_right} right results, on total of {how_many_found}"
         )
+    # # cleanup
+    # existing_objects = client.list_objects(
+    #     bucket_name, object_name, include_user_meta=True, include_version=True
+    # )
+    # for existing_object in existing_objects:
+    #     client.remove_object(bucket_name, object_name, existing_object.version_id)
+    # client.remove_bucket(bucket_name)
+    # return
 
+    logger.info(f"test 4: get specific version of an uploaded file")
+    image_rel_url = "%s/%s?versionId=%s" % (bucket_name, object_name, version_id)
+    image_url = urllib.parse.urljoin(storage_settings.document_storage_url, image_rel_url)
+    resp = urllib.request.urlopen(image_url)
+    if resp.status != 200:
+        logger.error(f"test 4 failed: not reachable {image_url}, response status {resp.status}")
+        return
+
+    text_download = resp.read()
+    if text_download != text:
+        logger.error(f"test 4 failed: {image_url} at download is different from the uploaded one")
+        logger.error(f"text2: {text_download[:100]}")
+        logger.error(f"text1: {text[:100]}")
+        logger.error(f"{image_url} has sha256: {hashlib.sha256(text_download).hexdigest()}")
+    else:
+        logger.info(f"test 4 passed")
+
+    # client.get_object(bucket_name, object_name, version_id=version_id)
     # cleanup
     existing_objects = client.list_objects(
         bucket_name, object_name, include_user_meta=True, include_version=True
     )
     for existing_object in existing_objects:
         client.remove_object(bucket_name, object_name, existing_object.version_id)
+    client.remove_bucket(bucket_name)
     return
