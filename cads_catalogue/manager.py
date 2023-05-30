165da55dc921dee4e3413f03df7916bf41819b24
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-05-30 11:13:42 +0200

    catalogue manager collect also field site from metadata.json

--- cads_catalogue/manager.py
+++ cads_catalogue/manager.py
@@ -313,6 +313,7 @@ def load_resource_metadata_file(folder_path: str | pathlib.Path) -> dict[str, An
     metadata["resource_update"] = data.get("update_date")
     if not metadata["resource_update"]:
         metadata["resource_update"] = data.get("publication_date")
+    metadata["site"] = data.get("site", "CDS")
     metadata["title"] = data.get("title")
     metadata["topic"] = data.get("topic")
     metadata["type"] = data.get("resource_type")
