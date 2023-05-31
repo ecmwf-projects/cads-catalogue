ae1ce2d93be56b70349cfee45d1d00ff5d63412b
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-05-31 16:57:43 +0200

    style

--- alembic/versions/8ccbe515155c_new_field_site.py
+++ alembic/versions/8ccbe515155c_new_field_site.py
@@ -1,26 +1,23 @@
-"""new field 'site'
+"""new field 'site'.
 
 Revision ID: 8ccbe515155c
 Revises: 472c0db250df
 Create Date: 2023-05-30 11:01:24.691827
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '8ccbe515155c'
-down_revision = '472c0db250df'
+revision = "8ccbe515155c"
+down_revision = "472c0db250df"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column(
-        "resources",
-        sa.Column("site", sa.String, index=True)
-    )
+    op.add_column("resources", sa.Column("site", sa.String, index=True))
 
 
 def downgrade() -> None:
