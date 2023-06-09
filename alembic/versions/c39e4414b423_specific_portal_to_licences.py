ab9291a6679103fd75e49bce6bae179dab7163ec
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-06-09 14:37:26 +0200

    style

--- alembic/versions/c39e4414b423_specific_portal_to_licences.py
+++ alembic/versions/c39e4414b423_specific_portal_to_licences.py
@@ -1,17 +1,17 @@
-"""specific portal to licences
+"""specific portal to licences.
 
 Revision ID: c39e4414b423
 Revises: 10bbb7ddf032
 Create Date: 2023-06-09 13:23:59.199638
 
 """
-import alembic
 import sqlalchemy as sa
 
+import alembic
 
 # revision identifiers, used by Alembic.
-revision = 'c39e4414b423'
-down_revision = '10bbb7ddf032'
+revision = "c39e4414b423"
+down_revision = "10bbb7ddf032"
 branch_labels = None
 depends_on = None
 
