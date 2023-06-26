0b56d092c171d6c9f29a0454773ccc87cf7e22d2
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-06-26 10:55:29 +0200

    migration update

---
+++ alembic/versions/ffb034573c96_dataset_sources_hash.py
@@ -0,0 +1,24 @@
+"""dataset sources hash.
+
+Revision ID: ffb034573c96
+Revises: c39e4414b423
+Create Date: 2023-06-26 10:50:52.670861
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "ffb034573c96"
+down_revision = "c39e4414b423"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("resources", sa.Column("sources_hash", sa.String))
+
+
+def downgrade() -> None:
+    op.drop_column("resources", "sources_hash")
