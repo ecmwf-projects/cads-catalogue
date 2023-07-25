3754734e263234b73265af1a8a871db0fb0951c5
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-07-25 16:13:37 +0200

    migration script update

---
+++ alembic/versions/e5e9a8a41828_add_new_field_adaptor_properties_hash.py
@@ -0,0 +1,24 @@
+"""add new field adaptor_properties_hash
+
+Revision ID: e5e9a8a41828
+Revises: ffb034573c96
+Create Date: 2023-07-25 16:07:47.059717
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e5e9a8a41828'
+down_revision = 'ffb034573c96'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("resources", sa.Column("adaptor_properties_hash", sa.String))
+
+
+def downgrade() -> None:
+    op.drop_column("resources", "adaptor_properties_hash")
