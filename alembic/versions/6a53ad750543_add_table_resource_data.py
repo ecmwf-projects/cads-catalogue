a09bb76cb5cedddb9285f45943be7a20521e15de
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-11-03 12:08:48 +0100

    style

--- alembic/versions/6a53ad750543_add_table_resource_data.py
+++ alembic/versions/6a53ad750543_add_table_resource_data.py
@@ -1,4 +1,4 @@
-"""add table resource_data
+"""add table resource_data.
 
 Revision ID: 6a53ad750543
 Revises: 4947a1be081e
@@ -7,12 +7,13 @@ Create Date: 2023-10-31 08:42:25.681640
 """
 
 import sqlalchemy as sa
-import alembic
 from sqlalchemy.dialects import postgresql as dialect_postgresql
 
+import alembic
+
 # revision identifiers, used by Alembic.
-revision = '6a53ad750543'
-down_revision = '4947a1be081e'
+revision = "6a53ad750543"
+down_revision = "4947a1be081e"
 branch_labels = None
 depends_on = None
 
@@ -25,13 +26,22 @@ def upgrade() -> None:
         sa.Column("constraints_data", dialect_postgresql.JSONB),
         sa.Column("form_data", dialect_postgresql.JSONB),
         sa.Column("mapping", dialect_postgresql.JSONB),
-        sa.Column("resource_uid", sa.String, sa.ForeignKey("resources.resource_uid"), nullable=False)
+        sa.Column(
+            "resource_uid",
+            sa.String,
+            sa.ForeignKey("resources.resource_uid"),
+            nullable=False,
+        ),
     )
-    alembic.op.create_unique_constraint("resource_uid_uc", "resource_data", ["resource_uid"])
-    sql = "INSERT INTO resource_data " \
-          "(adaptor_configuration, constraints_data, form_data, mapping, resource_uid) " \
-          "SELECT adaptor_configuration, constraints_data, form_data, mapping, resource_uid " \
-          "FROM resources"
+    alembic.op.create_unique_constraint(
+        "resource_uid_uc", "resource_data", ["resource_uid"]
+    )
+    sql = (
+        "INSERT INTO resource_data "
+        "(adaptor_configuration, constraints_data, form_data, mapping, resource_uid) "
+        "SELECT adaptor_configuration, constraints_data, form_data, mapping, resource_uid "
+        "FROM resources"
+    )
     conn = alembic.op.get_bind()
     conn.execute(sa.text(sql))
     alembic.op.drop_column("resources", "adaptor_configuration")
@@ -41,8 +51,12 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    alembic.op.add_column("resources", sa.Column("adaptor_configuration", dialect_postgresql.JSONB))
-    alembic.op.add_column("resources", sa.Column("constraints_data", dialect_postgresql.JSONB))
+    alembic.op.add_column(
+        "resources", sa.Column("adaptor_configuration", dialect_postgresql.JSONB)
+    )
+    alembic.op.add_column(
+        "resources", sa.Column("constraints_data", dialect_postgresql.JSONB)
+    )
     alembic.op.add_column("resources", sa.Column("form_data", dialect_postgresql.JSONB))
     alembic.op.add_column("resources", sa.Column("mapping", dialect_postgresql.JSONB))
     alembic.op.drop_table("resource_data")
