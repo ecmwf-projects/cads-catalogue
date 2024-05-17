0cdae716650396a523159f2f5b42c90aaabda2ea
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2024-05-17 13:32:21 +0200

    style

--- alembic/versions/654a874249a8_added_api_constraint_flag.py
+++ alembic/versions/654a874249a8_added_api_constraint_flag.py
@@ -5,20 +5,20 @@ Revises: 7366df17e57c
 Create Date: 2024-05-17 12:16:18.206589
 
 """
+
 import sqlalchemy as sa
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '654a874249a8'
-down_revision = '7366df17e57c'
+revision = "654a874249a8"
+down_revision = "7366df17e57c"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column(
-        "resources", sa.Column("api_enforce_constraints", sa.Boolean)
-    )
+    op.add_column("resources", sa.Column("api_enforce_constraints", sa.Boolean))
 
 
 def downgrade() -> None:
