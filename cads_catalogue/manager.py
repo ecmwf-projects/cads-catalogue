aa628fd3ee9b6431acd4f30bd8bc31830ceff161
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2025-02-21 14:26:29 +0100

    fixes some corner cases in case of exclude some entities

--- cads_catalogue/manager.py
+++ cads_catalogue/manager.py
@@ -670,14 +670,16 @@ def update_related_resources(session: sa.orm.session.Session):
 def prerun_processing(repo_paths, connection_string, filtering_kwargs) -> None:
     """Preliminary processing for the catalogue manager."""
     logger.info("additional input checks")
-    for repo_key, filter_key in [
-        ("cim_repo", "exclude_resources"),
-        ("licence_repo", "exclude_licences"),
-        ("message_repo", "exclude_messages"),
-        ("content_repo", "exclude_contents"),
+    for repo_key, filter_key, category in [
+        ("cim_repo", "exclude_resources", "resources"),
+        ("licence_repo", "exclude_licences", "licences"),
+        ("message_repo", "exclude_messages", "messages"),
+        ("content_repo", "exclude_contents", "contents"),
     ]:
         repo_path = repo_paths[repo_key]
         exclude = filtering_kwargs[filter_key]
+        if not repo_path and not exclude:
+            raise ValueError(f"missing required input folder for {category}")
         if not os.path.isdir(repo_path) and not exclude:
             raise ValueError(f"'{repo_path}' is not a folder")
     for repo_key, filter_key in [
@@ -685,7 +687,11 @@ def prerun_processing(repo_paths, connection_string, filtering_kwargs) -> None:
     ]:
         repo_paths_list = repo_paths[repo_key]
         exclude = filtering_kwargs[filter_key]
+        if not repo_paths_list and not exclude:
+            raise ValueError("missing required input folder for resources")
         for repo_path in repo_paths_list:
+            if not repo_path and not exclude:
+                raise ValueError("missing required input folder for resources")
             if not os.path.isdir(repo_path) and not exclude:
                 raise ValueError(f"'{repo_path}' is not a folder")
 
