c93bc16b9c0989ff45681d5996fa55ae2e360060
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2025-04-15 10:35:37 +0200

    refactoring+add local script run_sanity_check

--- cads_catalogue/manager.py
+++ cads_catalogue/manager.py
@@ -2,6 +2,12 @@
 
 import collections
 import datetime
+import glob
+import hashlib
+import itertools
+import json
+import os
+import pathlib
 
 # Copyright 2022, European Union.
 #
@@ -16,12 +22,8 @@ import datetime
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import glob
-import hashlib
-import itertools
-import json
-import os
-import pathlib
+from collections import defaultdict
+from operator import itemgetter
 from typing import Any, Dict, List, Sequence, Tuple
 
 import sqlalchemy as sa
@@ -29,6 +31,13 @@ import sqlalchemy_utils
 import structlog
 import yaml
 from sqlalchemy.dialects.postgresql import insert
+
+try:
+    import cads_e2e_tests
+except ImportError:
+    #  cads-e2e-tests not wanted in retrieve-api, cannot process sanity checks
+    pass
+
 
 from cads_catalogue import (
     config,
@@ -971,3 +980,97 @@ def list_all_repos_datasets(
         repo_datasets = list_repo_datasets(repo_path, filtering_kwargs)
         ret_value += repo_datasets
     return ret_value
+
+
+def report2sanity_check(report):
+    """Return a sanity check information from a cads-e2e-tests's report."""
+    sanity_check = {
+        "req_id": report.request_uid,
+        "success": not report.tracebacks,
+        "started_at": report.started_at.isoformat(),
+        "finished_at": report.finished_at.isoformat(),
+    }
+    return sanity_check
+
+
+def update_dataset_sanity_check(session_obj, dataset_uid, report, retain_only):
+    """Update sanity check column with a new outcome."""
+    current_sanity_check = report2sanity_check(report)
+    with session_obj.begin() as session:
+        dataset_obj = session.scalars(
+            sa.select(database.Resource).filter_by(resource_uid=dataset_uid)
+        ).first()
+        if not dataset_obj:
+            logger.error(
+                f"dataset uid {dataset_uid!r} in sanity check report was "
+                f"not found in the catalogue database!"
+            )
+            return
+        old_sanity_check = dataset_obj.sanity_check or []
+        new_sanity_check = old_sanity_check.append(current_sanity_check)
+        # note: sorting works with string if datetimes use isoformat!
+        new_sanity_check = sorted(
+            new_sanity_check, key=itemgetter("started_at"), reverse=True
+        )
+        if retain_only > 0:
+            new_sanity_check = new_sanity_check[:retain_only]
+        dataset_obj.sanity_check = new_sanity_check
+        session.add(dataset_obj)
+    logger.info(f"sanity check outcome stored for {dataset_uid!r}.")
+
+
+def update_sanity_checks_by_file(session_obj, report_path, retain_only):
+    """Update all sanity check columns by a reports file."""
+    with open(report_path) as fp:
+        reports = cads_e2e_tests.load_reports(fp)
+    dashboard_dict: Dict[str, List[Dict[str, Any]]] = defaultdict(list)
+    for report in reports:
+        dashboard_dict[report.request.collection_id].append(report2sanity_check(report))
+    with session_obj.begin() as session:
+        all_datasets = session.scalars(sa.select(database.Resource)).all()
+        for dataset in all_datasets:
+            if dataset.resource_uid in dashboard_dict:
+                additional_sanity_checks = dashboard_dict.pop(dataset.resource_uid)
+                old_sanity_check = dataset.sanity_check or []
+                new_sanity_check = old_sanity_check + additional_sanity_checks
+                # note: sorting works with string if datetimes use isoformat!
+                new_sanity_check = sorted(
+                    new_sanity_check, key=itemgetter("started_at"), reverse=True
+                )
+                if retain_only > 0:
+                    new_sanity_check = new_sanity_check[:retain_only]
+                dataset.sanity_check = new_sanity_check
+                session.add(dataset)
+            else:
+                logger.warning(
+                    f"dataset {dataset.resource_uid} not included in this sanity check report"
+                )
+    for dataset_uid in dashboard_dict:
+        logger.error(
+            f"dataset uid {dataset_uid!r} in sanity check report was "
+            f"not found in the catalogue database!"
+        )
+
+
+def run_sanity_check(session_obj, retain_only, **kwargs):
+    """Run e2e sanity checks and store outcomes."""
+    if "requests_path" in kwargs and kwargs["requests_path"] is not None:
+        requests_path = kwargs.pop("requests_path")
+        with open(requests_path, "r") as fp:
+            kwargs["requests"] = cads_e2e_tests.models.load_requests(fp)
+    else:
+        kwargs["requests"] = None
+    reports = []
+    for report in cads_e2e_tests.reports_generator(**kwargs):
+        reports.append(report)
+        dataset_uid = report.request.collection_id
+        if not dataset_uid:
+            continue
+        try:
+            update_dataset_sanity_check(session_obj, dataset_uid, report, retain_only)
+        except Exception:  # noqa
+            logger.exception(
+                f"sanity check outcome not stored for {dataset_uid!r}, error follows"
+            )
+            continue
+    cads_e2e_tests.cli.echo_passed_vs_failed(reports)
