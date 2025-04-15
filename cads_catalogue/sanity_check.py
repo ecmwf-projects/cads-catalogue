fb3b071983bc9ef6a27a2f469f4381a6271018dc
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2025-04-15 18:21:03 +0200

    moved json schemas in files apart

--- cads_catalogue/sanity_check.py
+++ cads_catalogue/sanity_check.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+import os
 from collections import defaultdict
 from operator import itemgetter
 from typing import Any, Dict, List
@@ -21,6 +23,7 @@ from typing import Any, Dict, List
 import jsonschema
 import sqlalchemy as sa
 import structlog
+import typer
 
 from cads_catalogue import database
 
@@ -32,36 +35,46 @@ except ImportError:
     #  cads-e2e-tests not wanted in retrieve-api, it cannot process sanity checks
     pass
 
+SCHEMA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "schemas")
 
-sanity_check_item_schema = {
-    "type": "object",
-    "properties": {
-        "req_id": {
-            "description": "The request uid of the test",
-            "type": "string"
-        },
-        "success": {
-            "description": "True if the test is successful",
-            "type": "boolean"
-        },
-        "started_at": {
-            "description": "ISO format of the date and time of the start of the test",
-            "type": "string"
-        },
-        "finished_at": {
-            "description": "ISO format of the date and time of the end of the test",
-            "type": "string"
-        }
-    },
-    "required": ["req_id", "success", "started_at", "finished_at"]
-}
+ref_mapping = {}
+for schema_def in ("sanity_check", "sanity_check_item"):
+    with open(os.path.join(SCHEMA_PATH, f"{schema_def}.json")) as fp:
+        ref_mapping[f"/schemas/{schema_def}"] = json.load(fp)
 
-sanity_check_schema = {
-    "type": "array",
-    "description": "list of sanity check outcomes on a dataset",
-    "items": sanity_check_item_schema,
-    "uniqueItems": True,
-}
+SanityCheckValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/sanity_check"]
+)
+SanityCheckItemValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/sanity_check_item"]
+)
+
+sanity_check_validator = SanityCheckValidator(
+    schema=ref_mapping["/schemas/sanity_check"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+sanity_check_item_validator = SanityCheckItemValidator(
+    schema=ref_mapping["/schemas/sanity_check_item"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+
+
+def echo_passed_vs_failed(reports: list[cads_e2e_tests.Report]) -> None:
+    """Print some statistics about reports."""
+    n_reports = len(reports)
+    typer.secho(
+        f"NUMBER OF REPORTS: {n_reports}",
+        fg=typer.colors.YELLOW if not n_reports else None,
+    )
+    if n_reports:
+        failed = sum(True for request in reports if request.tracebacks)
+        passed = len(reports) - failed
+        failed_perc = failed * 100 / n_reports
+        passed_perc = passed * 100 / n_reports
+        if failed:
+            typer.secho(f"FAILED: {failed} ({failed_perc:.1f}%)", fg=typer.colors.RED)
+        if passed:
+            typer.secho(f"PASSED: {passed} ({passed_perc:.1f}%)", fg=typer.colors.GREEN)
 
 
 def report2sanity_check(report: cads_e2e_tests.Report) -> dict[str, Any]:
@@ -72,11 +85,18 @@ def report2sanity_check(report: cads_e2e_tests.Report) -> dict[str, Any]:
         "started_at": report.started_at.isoformat(),
         "finished_at": report.finished_at.isoformat(),
     }
-    jsonschema.validate(sanity_check, schema=sanity_check_item_schema)
+    sanity_check_item_validator.validate(
+        sanity_check, ref_mapping["/schemas/sanity_check_item"]
+    )
     return sanity_check
 
 
-def update_dataset_sanity_check(session_obj: sa.orm.sessionmaker, dataset_uid: str, report: cads_e2e_tests.Report, retain_only: int) -> None:
+def update_dataset_sanity_check(
+    session_obj: sa.orm.sessionmaker,
+    dataset_uid: str,
+    report: cads_e2e_tests.Report,
+    retain_only: int,
+) -> None:
     """Update sanity check column with a new outcome."""
     current_sanity_check = report2sanity_check(report)
     with session_obj.begin() as session:
@@ -97,13 +117,17 @@ def update_dataset_sanity_check(session_obj: sa.orm.sessionmaker, dataset_uid: s
         )
         if retain_only > 0:
             new_sanity_check = new_sanity_check[:retain_only]
-        jsonschema.validate(new_sanity_check, schema=sanity_check_schema)
+        sanity_check_validator.validate(
+            new_sanity_check, ref_mapping["/schemas/sanity_check"]
+        )
         dataset_obj.sanity_check = new_sanity_check
         session.add(dataset_obj)
     logger.info(f"sanity check information updated for {dataset_uid!r}.")
 
 
-def update_sanity_checks_by_file(session_obj: sa.orm.sessionmaker, report_path: str, retain_only: int):
+def update_sanity_checks_by_file(
+    session_obj: sa.orm.sessionmaker, report_path: str, retain_only: int
+):
     """Update all sanity check columns by a reports file."""
     with open(report_path) as fp:
         reports = cads_e2e_tests.load_reports(fp)
@@ -138,7 +162,7 @@ def update_sanity_checks_by_file(session_obj: sa.orm.sessionmaker, report_path: 
 
 def run_sanity_check(session_obj: sa.orm.sessionmaker, retain_only: int, **kwargs):
     """Run e2e sanity checks and store outcomes."""
-    requests_path: str|None = kwargs.pop("requests_path", None)
+    requests_path: str | None = kwargs.pop("requests_path", None)
     if requests_path is not None:
         with open(requests_path, "r") as fp:
             kwargs["requests"] = cads_e2e_tests.models.load_requests(fp)
@@ -157,4 +181,4 @@ def run_sanity_check(session_obj: sa.orm.sessionmaker, retain_only: int, **kwarg
                 f"sanity check outcome not stored for {dataset_uid!r}, error follows"
             )
             continue
-    cads_e2e_tests.cli.echo_passed_vs_failed(reports)
+    echo_passed_vs_failed(reports)
