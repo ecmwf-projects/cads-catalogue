584bb43165081c3c345e00d2f1989a6072311eb3
Alfredo Toselli <alfredotoselli0@gmail.com>
2025-07-22 16:36:28 +0200

    chore: merge alembic heads

---
+++ alembic/versions/efce9b3eb861_merge_migration_heads.py
@@ -0,0 +1,21 @@
+"""merge migration heads.
+
+Revision ID: efce9b3eb861
+Revises: a81233756d0e, b7bbdaf01165
+Create Date: 2025-07-22 16:32:32.234648
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "efce9b3eb861"
+down_revision = ("a81233756d0e", "b7bbdaf01165")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
