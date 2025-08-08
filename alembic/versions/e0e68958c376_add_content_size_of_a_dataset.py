c860d529c9104b68ed8f6395ff8c7766692519dd
Alfredo Toselli <alfredotoselli0@gmail.com>
2025-08-08 10:49:09 +0200

    test: content_size field

--- alembic/versions/e0e68958c376_add_content_size_of_a_dataset.py
+++ alembic/versions/e0e68958c376_add_content_size_of_a_dataset.py
@@ -5,20 +5,21 @@ Revises: efce9b3eb861
 Create Date: 2025-08-07 10:26:09.831462
 
 """
-from alembic import op
+
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = 'e0e68958c376'
-down_revision = 'efce9b3eb861'
+revision = "e0e68958c376"
+down_revision = "efce9b3eb861"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('resources', sa.Column('content_size', sa.Integer(), nullable=True))
+    op.add_column("resources", sa.Column("content_size", sa.Integer(), nullable=True))
 
 
 def downgrade() -> None:
-    op.drop_column('resources', 'content_size')
+    op.drop_column("resources", "content_size")
